2cd56131dc6239656cd357d55b9ec7579593c13b
lexion <lexion@bk.ru>
2024-03-06 20:03:52 +0400

    Выполнение задания 5

---
+++ terraform/03/src/outputs.tf
@@ -0,0 +1,7 @@
+output "vm_output" {
+  value = [[for vm in concat(yandex_compute_instance.platform, values(yandex_compute_instance.platform_db) ) : {
+    instance_name = vm.name,
+    instance_id   = vm.id,
+    instance_fqdn = vm.fqdn
+  }]]
+}
