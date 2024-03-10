22d15c151b5bdf7ffdb1be6ffd830b0ebda0ad39
lexion <lexion@bk.ru>
2024-03-10 17:48:04 +0400

    Решено задание №2

--- terraform/04/demonstration1/main.tf
+++ terraform/04/demonstration1/main.tf
@@ -1,22 +1,19 @@
-#создаем облачную сеть
-resource "yandex_vpc_network" "develop" {
-  name = "develop"
+module "vpc_dev" {
+  source       = "./vpc"
+  env_name     = "develop"
+  zone = "ru-central1-a"
+  cidr = ["10.0.1.0/24"]
 }
 
-#создаем подсеть
-resource "yandex_vpc_subnet" "develop" {
-  name           = "develop-ru-central1-a"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["10.0.1.0/24"]
-}
+
+
 
 module "marketing_vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "develop"
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc_dev.network_id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  subnet_ids     = [module.vpc_dev.subnet_id]
   instance_name  = "web"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
@@ -34,9 +31,9 @@ module "marketing_vm" {
 module "analytics_vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "stage"
-  network_id     = yandex_vpc_network.develop.id
+  network_id     = module.vpc_dev.network_id
   subnet_zones   = ["ru-central1-a"]
-  subnet_ids     = [yandex_vpc_subnet.develop.id]
+  subnet_ids     = [module.vpc_dev.subnet_id]
   instance_name  = "web-stage"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
