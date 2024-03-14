f5cb12f1aaa80f5a4a727472a2824b46849abf7b
lexion <lexion@bk.ru>
2024-03-14 18:35:30 +0400

    Решена задача 4/05

---
+++ terraform/05/variables.tf
@@ -0,0 +1,21 @@
+variable "ip_address" {
+ type = string
+ default = "192.168.0.1"
+ description = "ip-адрес"
+ validation {
+   condition = can(cidrhost("${var.ip_address}/31", 1) == var.ip_address)
+   error_message = "Invalid ip address."
+} 
+}
+
+variable "list_ip_address" {
+ type = list(string)
+ default = ["192.168.0.1", "1.1.1.1", "1270.0.0.1"]
+ description = "ip-адрес"
+ validation {
+   condition = alltrue([
+     for ip in var.list_ip_address : can(cidrhost("${ip}/31", 1))
+   ])
+   error_message = "Invalid ip address in list."
+}
+}
